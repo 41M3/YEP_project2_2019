4d9927d74e1e339489802d6e5f0f79ad2fa10969
charles <charles.duthoit@epitech.eu>
2020-05-20 21:53:21 +0200

    [-] clean repo

--- API/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const user = mongoose.Schema({
-    code : String
-})
-
-module.exports = mongoose.model('user', user);
--- Delivery/public/js/getIp.js
+++ Delivery/public/js/getIp.js
@@ -5,7 +5,3 @@ xmlHttp.send( null );
 var div = document.getElementById('ip');
 var text = document.createTextNode(xmlHttp.responseText);
 div.appendChild(text);
-
-$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
-  console.log(JSON.stringify(data, null, 2));
-});
