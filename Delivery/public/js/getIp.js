4d854e09032823642c8482d57983813c61a28a61
charles <charles.duthoit@epitech.eu>
2020-05-20 20:47:54 +0200

    [*] change ip adress by jquery, and disp button in runner

--- Delivery/public/js/getIp.js
+++ Delivery/public/js/getIp.js
@@ -6,6 +6,6 @@ var div = document.getElementById('ip');
 var text = document.createTextNode(xmlHttp.responseText);
 div.appendChild(text);
 
-$.getJSON('https://json.geoiplookup.io/api?callback=?', function(data) {
+$.getJSON('http://gd.geobytes.com/GetCityDetails?callback=?', function(data) {
   console.log(JSON.stringify(data, null, 2));
 });
