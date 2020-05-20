898ef035cfddc943f8c50c825a42e650bb160754
Gaëtan <gaetan.chaugny@epitech.eu>
2020-05-20 04:14:29 +0200

    [+]runner : billy jumps, walks and everything. pfiouuu...

--- game/Runner/runner.js
+++ game/Runner/runner.js
@@ -1,57 +1,144 @@
-var canvas = document.getElementById("canvas"),
-    g = canvas.getContext("2d");
+buffer = document.createElement("canvas").getContext("2d");
+display = document.querySelector("canvas").getContext("2d");
 
-var width = window.innerWidth,
-    height = window.innerHeight,
-    ratio = window.devicePixelRatio;
+//billy fait du 16*16px
+var BILLY_SIZE = 16;
 
-canvas.width  = width  * ratio;
-canvas.height = height * ratio;
-canvas.style.width  = width  + "px";
-canvas.style.height = height + "px";
-g.scale(ratio, ratio);
-g.imageSmoothingEnabled = false;
-g.fillStyle = "rgba(255, 255, 255, 0.25)";
+function Animation(frame_set, delay) {  
+  	this.count = 0;
+  	this.delay = delay;
+  	this.frame = 0;
+  	this.frame_index = 0;
+  	this.frame_set = frame_set;
+};
 
-var x = width / 2,
-    r = 35,
-    stepRight = 0,
-    vx = r * 0.2;
+Animation.prototype = {
+	change:function(frame_set, delay = 15) {
+    	if (this.frame_set != frame_set) {
+      		this.count = 0;
+      		this.delay = delay;
+      		this.frame_index = 0;
+      		this.frame_set = frame_set;
+      		this.frame = this.frame_set[this.frame_index];
+    	}
+	},
 
-var spriteGuyRight = new Image();
-spriteGuyRight.onload = animate;
-spriteGuyRight.src = "guy-right.png";
+  	update:function() {
+    	this.count ++;
+    	if (this.count >= this.delay) {
+    	  	this.count = 0;
+    	  	this.frame_index = (this.frame_index == this.frame_set.length - 1) ? 0 : this.frame_index + 1;
+    	  	this.frame = this.frame_set[this.frame_index];
+    	}
+  	}
+};
 
-function animate() {
-    draw();
-    update();
-    requestAnimationFrame(animate);
+controller = {
+	left: { active:false, state:false },
+	right: { active:false, state:false },
+	up: { active:false, state:false },
+	
+	keyUpDown:function(event) {
+	  // Get the physical state of the key being pressed. true = down false = up
+		var key_state = (event.type == "keydown") ? true : false;
+	  	switch(event.keyCode) {
+			case 37:// gauche
+		  		if (controller.left.state != key_state) controller.left.active = key_state;
+		  			controller.left.state  = key_state;
+				break;
+			case 32: //up
+		  		if (controller.up.state != key_state) controller.up.active = key_state;
+		  			controller.up.state  = key_state;
+				break;
+			case 39:// droite
+		  		if (controller.right.state != key_state) 
+					controller.right.active = key_state;
+					controller.right.state  = key_state;
+			break;
+	  	}
+	}
+};
+
+player = { //Juste un rectangle avec un objet Animation
+	animation:new Animation(),
+	jumping:true,
+	height:16,    
+	width:16,
+	x:0,          
+	y:40 - 18,
+	x_velocity:0, 
+	y_velocity:0
+};
+
+//Sprite sheet
+billy = {
+	frame_sets:[[0, 1], [2, 3], [4, 5]],// immobile, gauche, droite
+	image:new Image()
+};
+
+function loop (time_stamp) {
+	if (controller.up.active && !player.jumping) {
+	  	controller.up.active = false;
+	  	player.jumping = true;
+	  	player.y_velocity -= 2.5;
+	}
+	if (controller.left.active) {
+	  	/* To change the animation, all you have to do is call animation.change. */
+	  	player.animation.change(billy.frame_sets[2], 15);
+	  	player.x_velocity -= 0.05;
+	}
+	if (controller.right.active) {
+	  	player.animation.change(billy.frame_sets[1], 15);
+	  	player.x_velocity += 0.05;
+	}
+	/* If you're just standing still, change the animation to standing still. */
+	if (!controller.left.active && !controller.right.active) {
+	  	player.animation.change(billy.frame_sets[0], 20);
+	}
+	player.y_velocity += 0.25;
+	player.x += player.x_velocity;
+	player.y += player.y_velocity;
+	player.x_velocity *= 0.9;
+	player.y_velocity *= 0.9;
+	if (player.y + player.height > buffer.canvas.height - 2) {
+	  	player.jumping = false;
+	  	player.y = buffer.canvas.height - 2 - player.height;
+	  	player.y_velocity = 0;
+	}
+	if (player.x + player.width < 0) {
+	  	player.x = buffer.canvas.width;
+	} else if (player.x > buffer.canvas.width) {
+	  	player.x = - player.width;
+	}
+	player.animation.update();
+	render();
+	window.requestAnimationFrame(loop);
+};
+
+function render() {
+	buffer.fillRect(0, 0, buffer.canvas.width, buffer.canvas.height);
+	buffer.fillStyle = "#009900";
+	
+	buffer.drawImage(billy.image, player.animation.frame * BILLY_SIZE, 0, BILLY_SIZE, BILLY_SIZE, Math.floor(player.x), Math.floor(player.y), BILLY_SIZE, BILLY_SIZE);
+	display.drawImage(buffer.canvas, 0, 0, buffer.canvas.width, buffer.canvas.height, 0, 0, display.canvas.width, display.canvas.height);
+};
+
+function resize() {
+    display.canvas.width = document.documentElement.clientWidth - 32;
+  	if (display.canvas.width > document.documentElement.clientHeight) {
+    	display.canvas.width = document.documentElement.clientHeight;
+  	}
+  	display.canvas.height = display.canvas.width * 0.5;
+  	display.imageSmoothingEnabled = false;
 }
 
-function draw() {
-    g.fillRect(0, 0, width, height);
+buffer.canvas.width = 160;
+buffer.canvas.height = 80;
+window.addEventListener("resize", resize);
+window.addEventListener("keydown", controller.keyUpDown);
+window.addEventListener("keyup", controller.keyUpDown);
 
-    // Affichage
-    runningRight(x, height, r, Math.floor(stepRight));
-}
+resize();
 
-function runningRight(x, y, r, stepRight) {
-    var s = r/9;
-
-    // params : source, sx, sy, sw, sh, dx, dy, dw, dh
-    g.drawImage(spriteGuyRight, 32*stepRight, 0, 30, 55, x - 16*s, y - 65*s, 30*s, 55*s);
-}
-
-
-function update() {
-    // Mise à jour des variables
-    x += vx;
-    
-    // if (x < r || x > width - r) { rebondissement
-    //     vx *= -1;
-    // }
-    
-    stepRight += 0.3;
-    if (stepRight >= 9)
-        stepRight -= 9;
-}
+window.requestAnimationFrame(loop);// game loop 
+billy.image.src = "guy.png";
