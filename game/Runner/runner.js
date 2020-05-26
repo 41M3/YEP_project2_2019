5db30f1d47a3873c2bbd66ca60d0662f3d80122e
Gaëtan <gaetan.chaugny@epitech.eu>
2020-05-26 17:39:15 +0200

    [+]debug jump animation + create infinite running animation

--- game/Runner/runner.js
+++ game/Runner/runner.js
@@ -1,144 +1,138 @@
-buffer = document.createElement("canvas").getContext("2d");
-display = document.querySelector("canvas").getContext("2d");
+(function() { "use strict";
 
-//billy fait du 16*16px
-var BILLY_SIZE = 16;
+  const TILE_SIZE = 16;
+  const WORLD_HEIGHT = 144;
+  const WORLD_WIDTH = 256;
 
-function Animation(frame_set, delay) {  
-  	this.count = 0;
-  	this.delay = delay;
-  	this.frame = 0;
-  	this.frame_index = 0;
-  	this.frame_set = frame_set;
-};
+  //// CLASSES ////
 
-Animation.prototype = {
-	change:function(frame_set, delay = 15) {
-    	if (this.frame_set != frame_set) {
-      		this.count = 0;
-      		this.delay = delay;
-      		this.frame_index = 0;
-      		this.frame_set = frame_set;
-      		this.frame = this.frame_set[this.frame_index];
-    	}
-	},
+  var Animation = function(frame_set, delay) {
 
-  	update:function() {
-    	this.count ++;
-    	if (this.count >= this.delay) {
-    	  	this.count = 0;
-    	  	this.frame_index = (this.frame_index == this.frame_set.length - 1) ? 0 : this.frame_index + 1;
-    	  	this.frame = this.frame_set[this.frame_index];
-    	}
-  	}
-};
+    this.count = 0;// Counts the number of game cycles since the last frame change.
+    this.delay = delay;// The number of game cycles to wait until the next frame change.
+    this.frame_value = frame_set[0];// The value in the sprite sheet of the sprite image / tile to display.
+    this.frame_index = 0;// The frame's index in the current animation frame set.
+    this.frame_set = frame_set;// The current animation frame set that holds sprite tile values.
 
-controller = {
-	left: { active:false, state:false },
-	right: { active:false, state:false },
-	up: { active:false, state:false },
-	
-	keyUpDown:function(event) {
-	  // Get the physical state of the key being pressed. true = down false = up
-		var key_state = (event.type == "keydown") ? true : false;
-	  	switch(event.keyCode) {
-			case 37:// gauche
-		  		if (controller.left.state != key_state) controller.left.active = key_state;
-		  			controller.left.state  = key_state;
-				break;
-			case 32: //up
-		  		if (controller.up.state != key_state) controller.up.active = key_state;
-		  			controller.up.state  = key_state;
-				break;
-			case 39:// droite
-		  		if (controller.right.state != key_state) 
-					controller.right.active = key_state;
-					controller.right.state  = key_state;
-			break;
-	  	}
-	}
-};
+  };
 
-player = { //Juste un rectangle avec un objet Animation
-	animation:new Animation(),
-	jumping:true,
-	height:16,    
-	width:16,
-	x:0,          
-	y:40 - 18,
-	x_velocity:0, 
-	y_velocity:0
-};
+  Animation.prototype = {
 
-//Sprite sheet
-billy = {
-	frame_sets:[[0, 1], [2, 3], [4, 5]],// immobile, gauche, droite
-	image:new Image()
-};
+    /* This changes the current animation frame set. For example, if the current
+    set is [0, 1], and the new set is [2, 3], it changes the set to [2, 3]. It also
+    sets the delay. */
+    change:function(frame_set, delay = 15) {
 
-function loop (time_stamp) {
-	if (controller.up.active && !player.jumping) {
-	  	controller.up.active = false;
-	  	player.jumping = true;
-	  	player.y_velocity -= 2.5;
-	}
-	if (controller.left.active) {
-	  	/* To change the animation, all you have to do is call animation.change. */
-	  	player.animation.change(billy.frame_sets[2], 15);
-	  	player.x_velocity -= 0.05;
-	}
-	if (controller.right.active) {
-	  	player.animation.change(billy.frame_sets[1], 15);
-	  	player.x_velocity += 0.05;
-	}
-	/* If you're just standing still, change the animation to standing still. */
-	if (!controller.left.active && !controller.right.active) {
-	  	player.animation.change(billy.frame_sets[0], 20);
-	}
-	player.y_velocity += 0.25;
-	player.x += player.x_velocity;
-	player.y += player.y_velocity;
-	player.x_velocity *= 0.9;
-	player.y_velocity *= 0.9;
-	if (player.y + player.height > buffer.canvas.height - 2) {
-	  	player.jumping = false;
-	  	player.y = buffer.canvas.height - 2 - player.height;
-	  	player.y_velocity = 0;
-	}
-	if (player.x + player.width < 0) {
-	  	player.x = buffer.canvas.width;
-	} else if (player.x > buffer.canvas.width) {
-	  	player.x = - player.width;
-	}
-	player.animation.update();
-	render();
-	window.requestAnimationFrame(loop);
-};
+      if (this.frame_set != frame_set) {// If the frame set is different:
 
-function render() {
-	buffer.fillRect(0, 0, buffer.canvas.width, buffer.canvas.height);
-	buffer.fillStyle = "#009900";
-	
-	buffer.drawImage(billy.image, player.animation.frame * BILLY_SIZE, 0, BILLY_SIZE, BILLY_SIZE, Math.floor(player.x), Math.floor(player.y), BILLY_SIZE, BILLY_SIZE);
-	display.drawImage(buffer.canvas, 0, 0, buffer.canvas.width, buffer.canvas.height, 0, 0, display.canvas.width, display.canvas.height);
-};
+        this.count = 0;// Reset the count.
+        this.delay = delay;// Set the delay.
+        this.frame_index = 0;// Start at the first frame in the new frame set.
+        this.frame_set = frame_set;// Set the new frame set.
+        this.frame_value = this.frame_set[this.frame_index];// Set the new frame value.
 
-function resize() {
-    display.canvas.width = document.documentElement.clientWidth - 32;
-  	if (display.canvas.width > document.documentElement.clientHeight) {
-    	display.canvas.width = document.documentElement.clientHeight;
-  	}
-  	display.canvas.height = display.canvas.width * 0.5;
-  	display.imageSmoothingEnabled = false;
-}
+      }
 
-buffer.canvas.width = 160;
-buffer.canvas.height = 80;
-window.addEventListener("resize", resize);
-window.addEventListener("keydown", controller.keyUpDown);
-window.addEventListener("keyup", controller.keyUpDown);
+    },
 
-resize();
+    /* Call this on each game cycle. */
+    update:function() {
 
-window.requestAnimationFrame(loop);// game loop 
-billy.image.src = "guy.png";
+      this.count ++;// Keep track of how many cycles have passed since the last frame change.
+
+      if (this.count >= this.delay) {// If enough cycles have passed, we change the frame.
+
+        this.count = 0;// Reset the count.
+        /* If the frame index is on the last value in the frame set, reset to 0.
+        If the frame index is not on the last value, just add 1 to it. */
+        this.frame_index = (this.frame_index == this.frame_set.length - 1) ? 0 : this.frame_index + 1;
+        this.frame_value = this.frame_set[this.frame_index];// Change the current frame value.
+
+      }
+
+    }
+
+  };
+
+  /* A frame just keeps track of a physical position inside the tile sheet for blitting. */
+  var Frame = function(x, y, width, height) {
+
+    this.height = height;
+    this.width  = width;
+    this.x      = x;
+    this.y      = y;
+
+  };
+
+  /* A Pool object manages objects. The objects array holds all objects that are
+  currently in use, and the pool holds objects that are not in use. By storing objects
+  that would otherwise be deleted, we can reuse them instead of creating totally new
+  instances with the new operator. Recycling saves memory. Do it. */
+  var Pool = function(object) {
+
+    this.object = object;// The constructor of the object we are pooling.
+    this.objects = [];// The array of objects in use.
+    this.pool = [];// The array of objects not in use.
+
+  };
+
+  Pool.prototype = {
+
+    /* Get an object from the pool or create a new object. Pool expects objects to
+    have a few basic functions, like reset. */
+    get:function(parameters) {
+
+      if (this.pool.length != 0) {
+
+        let object = this.pool.pop();
+        object.reset(parameters);
+        this.objects.push(object);
+
+      } else {
+
+        this.objects.push(new this.object(parameters.x, parameters.y));
+
+      }
+
+    },
+
+    store:function(object) {
+
+      let index = this.objects.indexOf(object);
+
+      if (index != -1) {
+
+        this.pool.push(this.objects.splice(index, 1)[0]);
+
+      }
+
+    },
+
+    storeAll:function() {
+
+      for (let index = this.objects.length - 1; index > -1; -- index) {
+
+        this.pool.push(this.objects.pop());
+
+      }
+
+    }
+
+  };
+
+  var Meteor = function(x, y) {
+
+    this.alive = true;// Meteor dies when it goes offscreen.
+    this.animation = new Animation(display.tile_sheet.frame_sets[1], 8);
+    this.grounded = false;
+    this.smoke = false;// smoke values are used for spawning smoke particles.
+    this.smoke_count = 0;
+    this.smoke_delay = Math.floor(Math.random() * 10 + 5);
+    this.height = Math.floor(Math.random() * 16 + 24); this.width = this.height;
+    this.x = x; this.y = y - this.height * 0.5;
+    let direction = Math.PI * 1.75 + Math.random() * Math.PI * 0.1;// The trajectory.
+    this.x_velocity = Math.cos(direction) * 3; this.y_velocity = -Math.sin(direction) * 3;
+
+  };
+
+  
